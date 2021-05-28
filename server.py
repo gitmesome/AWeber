85851f386d4e40c653602f76235a7649ab8d5a22
Christian Ullman <christian.ullman@endurance.com>
2021-05-28 11:06:38 -0600

    Crossed swaggar and openapi
    fixed  typeo in server.py
    
    `python3 server.py`  Launches the flax-connexion app

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import config
 connex_app = config.connex_app
 
 # Read the swagger.yml file to configure the endpoints
-connex_app.add_api("openapi-spec.yml")
+connex_app.add_api("openapi-spec.yaml")
 
 if __name__ == "__main__":
     connex_app.run(debug=True)
