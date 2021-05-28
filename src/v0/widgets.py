2fb7b386e684ef16b11d1a63feca65ed0715d415
Christian Ullman <christian.ullman@endurance.com>
2021-05-27 22:37:29 -0600

    running linter - flake8

--- src/v0/widgets.py
+++ src/v0/widgets.py
@@ -1,15 +1,18 @@
-
 def read_all():
     pass
+
 
 def read_one(widget_id):
     pass
 
+
 def create(widget):
     pass
 
+
 def update(widget_id, widget):
     pass
+
 
 def delete(widget_id):
     pass
