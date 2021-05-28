319febffa467e8e8b3d041819952f3baf1724668
Christian Ullman <christian.ullman@endurance.com>
2021-05-27 22:29:04 -0600

    adding server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+# Local modules
+import config
+
+# Get the application instance
+connex_app = config.connex_app
+
+# Read the swagger.yml file to configure the endpoints
+connex_app.add_api("openapi-spec.yml")
+
+if __name__ == "__main__":
+    connex_app.run(debug=True)
